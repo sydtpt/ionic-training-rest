097b85cd489ec94ae88957aa7ac074a5472f20c1
Sydnei Tanikawa <sydtpt@gmail.com>
2018-04-15 10:59:37 -0300

    'ordernacao'

--- api/entities/feed/feed-routes.js
+++ api/entities/feed/feed-routes.js
@@ -2,7 +2,7 @@ module.exports = (function() {
     var router = require('express').Router();
     let Post = require('../post/post');
     router.get('/',function(req,res){
-        Post.find({},function(err, posts){
+        Post.find({}).sort('-updatedAt').exec(function(err, posts){
             res.status(200);
             res.send(posts);
         });
