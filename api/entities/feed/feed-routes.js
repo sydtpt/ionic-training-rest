c8dd9d678c0469cc46374aa6b3ff50ac0237d82b
Sydnei Tanikawa <sydtpt@gmail.com>
2018-04-19 22:26:31 -0300

    ajuste no retorno dos posts

--- api/entities/feed/feed-routes.js
+++ api/entities/feed/feed-routes.js
@@ -10,8 +10,24 @@ module.exports = (function() {
 
         Post.find(filter).sort('-createdAt').limit(3).exec(function(err, posts){
             res.status(200);
+            let response = {
+                posts:posts 
+            }
             res.send(posts);
         });
     });
+
+    router.delete('/',function(req,res){
+        Post.remove({});
+        res.status(200);
+        res.send('All posts has been deleted!');
+    });
+
+    router.delete('/:id',function(req,res){
+        Post.findOneAndRemove({_id: req.path.id});
+        res.status(200);
+        res.send('All posts has been deleted!');
+    });
+
     return router;
 })();
